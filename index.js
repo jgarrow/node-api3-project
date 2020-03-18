c5a447de139616aca7f6f3df46e8a11bb86c9a41
Janessa Garrow <janessa.garrow@gmail.com>
2020-03-17 19:23:30 -0600

    Create custom logger middleware

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code away!
+const express = require("express");
+
+const server = express();
+const port = 5000;
+
+const logger = (req, res, next) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} to ${req.url}`);
+
+    next();
+};
+
+server.use(express.json());
+server.use(logger);
+
+server.listen(port, () => console.log(`Server listening on port ${port}`));
